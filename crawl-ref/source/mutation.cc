2ef29f82df6571148bb5a600c67fd2f4ab3ac149
Jeremy Gurr <jeremy.gurr@gmail.com>
2016-04-30 14:29:22 -0700

    fix depends mutation bug

--- crawl-ref/source/mutation.cc
+++ crawl-ref/source/mutation.cc
@@ -78,7 +78,7 @@ enum class mutflag
     XOM     = 1 << 4, // xom being xom
     DEPENDS = 1 << 5, // maybe good, maybe bad depending on the circumstances
 
-    LAST    = XOM
+    LAST    = DEPENDS,
 };
 DEF_BITFIELD(mutflags, mutflag, 4);
 COMPILE_CHECK(mutflags::exponent(mutflags::last_exponent) == mutflag::LAST);
@@ -888,7 +888,9 @@ static mutation_type _get_random_mutation(mutation_type mutclass)
         {
             const int good_dna = player_mutation_level(MUT_GOOD_DNA);
             const int bad_dna = player_mutation_level(MUT_BAD_DNA);
-            mt = x_chance_in_y(3 + random2(good_dna * 2) - random2(bad_dna * 2), 5) ? mutflag::GOOD : mutflag::BAD;
+            mt = x_chance_in_y(3 + random2(good_dna * 2) - random2(bad_dna * 2), 5)
+                 ? mutflag::GOOD
+                 : (x_chance_in_y(1, 3) ? mutflag::DEPENDS : mutflag::BAD);
         }
             break;
         case RANDOM_BAD_MUTATION:
