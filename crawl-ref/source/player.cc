54a6c834b1cdb01dbc45e446106c114650d817b9
Jeremy Gurr <jeremy.gurr@gmail.com>
2016-05-10 09:53:35 -0700

    double summoning cost

--- crawl-ref/source/player.cc
+++ crawl-ref/source/player.cc
@@ -9410,7 +9410,7 @@ int player_spell_mp_freeze_modifier(spell_type which_spell, bool raw, int old_co
 
     if (is_summon_spell(which_spell))
     {
-        new_cost = spell_difficulty(which_spell) * 4;
+        new_cost = spell_difficulty(which_spell) * 8;
     }
 
     new_cost = _apply_hunger(which_spell, new_cost);
