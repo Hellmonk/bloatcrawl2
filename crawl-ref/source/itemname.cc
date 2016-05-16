3b7ca139b6d60502c132f599cf66bd4fb2483e1e
Jeremy Gurr <jeremy.gurr@gmail.com>
2016-05-16 13:19:34 -0700

    cosmetic change

--- crawl-ref/source/itemname.cc
+++ crawl-ref/source/itemname.cc
@@ -787,7 +787,7 @@ const char* jewellery_effect_name(int jeweltype, bool terse)
         case AMU_GUARDIAN_SPIRIT:   return "guardian spirit";
         case AMU_FAITH:             return "faith";
         case AMU_REFLECTION:        return "reflection";
-        case AMU_REGENERATION:      return "regeneration";
+        case AMU_REGENERATION:      return "health regeneration";
         default: return "buggy jewellery";
         }
     }
