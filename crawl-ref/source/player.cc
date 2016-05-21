3e42f830f642c119761d79840497cc16407d2a8e
Jeremy Gurr <jeremy.gurr@gmail.com>
2016-05-21 09:18:05 -0700

    fix cursing of unwearable slot bug

--- crawl-ref/source/player.cc
+++ crawl-ref/source/player.cc
@@ -696,6 +696,7 @@ void update_vision_range()
  */
 maybe_bool you_can_wear(equipment_type eq, bool temp)
 {
+    eq = EQ_RING_THREE;
     if (temp && !get_form()->slot_available(eq))
         return MB_FALSE;
 
