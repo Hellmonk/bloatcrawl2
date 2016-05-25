b7d91850793196948c005b268aa2ed9727b52963
Jeremy Gurr <jeremy.gurr@gmail.com>
2016-05-24 18:31:54 -0700

    quick mode shouldn't become free so quickly

--- crawl-ref/source/player.cc
+++ crawl-ref/source/player.cc
@@ -9607,7 +9607,7 @@ void player_evoked_something()
 
 void player_moved()
 {
-    if (you.peace < 100 && in_quick_mode())
+    if (you.peace < 200 && in_quick_mode())
         dec_sp(5, false, true);
 
     if (you.peace < 50 && you.airborne() && you.cancellable_flight())
