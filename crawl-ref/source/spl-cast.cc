1b8f87c149aa2bd5fb9fc80e9566530bee94ea7a
Jeremy Gurr <jeremy.gurr@gmail.com>
2016-05-25 09:50:11 -0700

    failure tweak

--- crawl-ref/source/spl-cast.cc
+++ crawl-ref/source/spl-cast.cc
@@ -514,13 +514,13 @@ int raw_spell_fail(spell_type spell)
     x += spellcasting_penalty * -0.3;
     x += anti_wizardry * -3;
     x += vertigo * -3;
-    x += wild * -5;
-    x += subdued * 5;
+    x += wild * -10;
+    x += subdued * 10;
     x += dex;
     x += spellcasting_skill / 10.0;
     x += school_average / 10.0;
     x += high_council;
-    x += player_success;
+    x += player_success * 2;
     x += success_boosts;
 
     int fail_chance = 100 * pow(15.0 / 16, abs(x)) / 2;
