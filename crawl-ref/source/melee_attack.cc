57b0347aee69049e28bc8c812ac9440fb12510a4
Jeremy Gurr <jeremy.gurr@gmail.com>
2016-06-08 11:07:48 -0700

    remove cleaving directional damage reduction

--- crawl-ref/source/melee_attack.cc
+++ crawl-ref/source/melee_attack.cc
@@ -3497,12 +3497,13 @@ void melee_attack::cleave_setup()
     cleave_targets.pop_front();
 }
 
-// cleave damage modifier for additional attacks: 70% of base damage
+// cleave damage modifier for additional attacks (was 70%)
 int melee_attack::cleave_damage_mod(int dam)
 {
+    const int percentage_of_damage = 100;
     if (weapon && is_unrandom_artefact(*weapon, UNRAND_GYRE))
         return dam;
-    return div_rand_round(dam * 7, 10);
+    return div_rand_round(dam * percentage_of_damage, 100);
 }
 
 void melee_attack::chaos_affect_actor(actor *victim)
