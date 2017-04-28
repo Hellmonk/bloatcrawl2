286b805c5d0fe147659a1f63f07705de79572f36
hellmonk <nldehaan91@gmail.com>
2017-04-27 20:12:14 -0500

    Remove scythes. Except the scythe of curses. Sigmund and reapers get their scythes implicitly, gaining AF_REACH and a boost to their base damage.

--- crawl-ref/source/itemprop.cc
+++ crawl-ref/source/itemprop.cc
@@ -556,7 +556,7 @@ static const weapon_def Weapon_prop[] =
         DAMV_CHOPPING | DAM_PIERCE, 5, 10, 40, POLEARM_BRANDS },
     { WPN_SCYTHE,            "scythe",             14, -4, 20,
         SK_POLEARMS,     SIZE_MEDIUM, NUM_SIZE_LEVELS, MI_NONE,
-        DAMV_SLICING, 2, 0, 30, POLEARM_BRANDS },
+        DAMV_SLICING, 0, 0, 0, POLEARM_BRANDS },
     { WPN_DEMON_TRIDENT,     "demon trident",      12,  1, 13,
         SK_POLEARMS,     SIZE_LITTLE, SIZE_LITTLE, MI_NONE,
         DAMV_PIERCING, 0, 2, 150, DEMON_BRANDS },
