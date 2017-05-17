bb22f47b880de377f1036a3c7aa41b1e5a78016f
hellmonk <nldehaan91@gmail.com>
2017-05-16 22:11:23 -0500

    try to fix a staff acquirement bug

--- crawl-ref/source/makeitem.cc
+++ crawl-ref/source/makeitem.cc
@@ -1440,19 +1440,6 @@ static void _generate_book_item(item_def& item, bool allow_uniques,
 static void _generate_staff_item(item_def& item, bool allow_uniques,
                                  int force_type, int item_level, int agent)
 {
-    // If we make the unique roll, no further generation necessary.
-    // Copied unrand code from _try_make_weapon_artefact since randart enhancer staves
-    // can't happen.
-    if (allow_uniques
-        && one_chance_in(item_level == ISPEC_GOOD_ITEM ? 27 : 100))
-    {
-        // Temporarily fix the base_type to get enhancer staves
-        item.base_type = OBJ_WEAPONS;
-        if (_try_make_item_unrand(item, WPN_STAFF, agent))
-            return;
-        item.base_type = OBJ_STAVES;
-    }
-
     if (force_type == OBJ_RANDOM)
     {
         do
@@ -1464,8 +1451,6 @@ static void _generate_staff_item(item_def& item, bool allow_uniques,
     else
         item.sub_type = force_type;
 
-    if (one_chance_in(16))
-        do_curse_item(item);
 }
 
 static void _generate_rune_item(item_def& item, int force_type)
