ed06e5269e0384843c1c07edc638cd2a380281f0
hellmonk <nldehaan91@gmail.com>
2017-09-23 11:53:44 -0500

    fix kick aux damage

--- crawl-ref/source/melee_attack.cc
+++ crawl-ref/source/melee_attack.cc
@@ -981,18 +981,18 @@ public:
         if (you.has_usable_hooves())
         {
             // Max hoof damage: 10.
-            return you.get_mutation_level(MUT_HOOVES) * 5 / 3;
+            return damage + you.get_mutation_level(MUT_HOOVES) * 5 / 3;
         }
 
         if (you.has_usable_talons())
         {
             // Max talon damage: 9.
-            return 1 + you.get_mutation_level(MUT_TALONS);
+            return damage + 1 + you.get_mutation_level(MUT_TALONS);
         }
 
         // Max spike damage: 8.
         // ... yes, apparently tentacle spikes are "kicks".
-        return you.get_mutation_level(MUT_TENTACLE_SPIKE) * 3;
+        return damage + you.get_mutation_level(MUT_TENTACLE_SPIKE) * 3;
     }
 
     string get_verb() const override
