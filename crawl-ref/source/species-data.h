572bfbbb4858f92ccc14e1dd679b0dec68cbe714
hellmonk <nldehaan91@gmail.com>
2017-11-10 20:59:13 -0600

    make spriggans sanic again

--- crawl-ref/source/species-data.h
+++ crawl-ref/source/species-data.h
@@ -536,7 +536,7 @@ static const map<species_type, species_def> species_data =
     HT_LAND, US_ALIVE, SIZE_LITTLE,
     4, 9, 11, // 24
     { STAT_INT, STAT_DEX }, 5,
-    { { MUT_FAST, 2, 1 }, { MUT_ACUTE_VISION, 1, 1 }, },
+    { { MUT_FAST, 3, 1 }, { MUT_ACUTE_VISION, 1, 1 }, },
     {},
     {},
     { JOB_ARTIFICER, JOB_ENCHANTER, JOB_EARTH_ELEMENTALIST, JOB_ARCANE_MARKSMAN },
