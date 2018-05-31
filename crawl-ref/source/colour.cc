b0bd913611b5032718e29f91bbf22b2801516f77
hellmonk <nldehaan91@gmail.com>
2018-05-30 21:59:54 -0400

    recolor awoken forests in console (ebering)

--- crawl-ref/source/colour.cc
+++ crawl-ref/source/colour.cc
@@ -652,8 +652,8 @@ void init_element_colours()
                         0));
     add_element_colour(_create_random_element_colour_calc(
                             ETC_AWOKEN_FOREST, "awoken_forest",
-                            40, GREEN,
-                            40, LIGHTGREEN,
+                            40, RED,
+                            40, LIGHTRED,
                         0));
     add_element_colour(new random_element_colour_calc(
                             ETC_WU_JIAN, "wu_jian",
