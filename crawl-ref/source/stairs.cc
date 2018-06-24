8b22c7b725f6c5e31496beb8ce80e475f85f5b61
Chris Campbell <chriscampbell89@gmail.com>
2018-06-24 22:35:29 +0100

    Don't cause barb damage on forced level transitions
    Such as being banished or falling down an unknown shaft. Also re-order some
    checks.
    
    (cherry picked from commit 334a0db03a1abb78e9a5149a6690f777404b288e)

--- crawl-ref/source/stairs.cc
+++ crawl-ref/source/stairs.cc
@@ -556,7 +556,8 @@ static level_id _travel_destination(const dungeon_feature_type how,
  */
 void floor_transition(dungeon_feature_type how,
                       const dungeon_feature_type whence, level_id whither,
-                      bool forced, bool going_up, bool shaft, bool update_travel_cache)
+                      bool forced, bool going_up, bool shaft,
+                      bool update_travel_cache)
 {
     const level_id old_level = level_id::current();
 
@@ -567,11 +568,14 @@ void floor_transition(dungeon_feature_type how,
     clear_trapping_net();
     end_searing_ray();
 
+    if (!forced)
+    {
+        // Check for barbs and apply
+        apply_barbs_damage();
+    }
+
     // Fire level-leaving trigger.
     leaving_level_now(how);
-
-    //Check for barbs and apply
-    apply_barbs_damage();
 
     // Not entirely accurate - the player could die before
     // reaching the Abyss.
