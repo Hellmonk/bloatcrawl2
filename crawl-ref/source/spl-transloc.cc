dd26bb6066ce5dfc8e03bbb5b035271e6445b23b
NormalPerson7 <cmrn.stphnsn@hotmail.com>
2018-09-23 18:29:17 +0100

    Unify range checks between the targeter and fatal_attraction for Gell's
    This commit:
     (a) re-implements a gravitas_range function to be used by both the
         fatal_attraction function and the targeter for Gell's Gravitas;
     (b) makes this function return a value (2 or 3) solely dependent on
         whether the spellpower is greater than the breakpoint of 80 rather
         than using an overcomplicated and inaccurate formula to do this;
     (c) makes the fatal_attraction function check the range of an actor
         near to the centre of the "gravitational reorientation" using the
         gravitas_range function instead of checking if the calculated
         strength was greater than 0; and
     (d) calculates the strength as before in fatal_attraction, but removes
         the restriction that it must always be less than LOS_RADIUS - this
         sanity check was unnecessary and limited the damage slightly at
         high power for targets colliding when they are range 1 from the
         centre.

--- crawl-ref/source/spl-transloc.cc
+++ crawl-ref/source/spl-transloc.cc
@@ -1033,6 +1033,12 @@ spret_type cast_dispersal(int pow, bool fail)
     return SPRET_SUCCESS;
 }
 
+int gravitas_range(int pow)
+{
+    return pow >= 80 ? 3 : 2;
+}
+
+
 #define GRAVITY "by gravitational forces"
 
 static void _attract_actor(const actor* agent, actor* victim,
@@ -1096,10 +1102,10 @@ bool fatal_attraction(const coord_def& pos, const actor *agent, int pow)
             continue;
 
         const int range = (pos - ai->pos()).rdist();
-        const int strength =
-            min(LOS_RADIUS, ((pow + 100) / 20) / (range*range));
-        if (strength <= 0)
+        if (range > gravitas_range(pow))
             continue;
+
+        const int strength = ((pow + 100) / 20) / (range*range);
 
         affected = true;
         _attract_actor(agent, *ai, pos, pow, strength);
