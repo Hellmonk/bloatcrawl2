647bf2579a59f452f06a9ec98d9485c35f679d92
Alan Malloy <alan@malloys.org>
2018-11-29 09:33:31 -0800

    Mark some local functions as static to fix compiler warnings

--- crawl-ref/source/spl-goditem.cc
+++ crawl-ref/source/spl-goditem.cc
@@ -266,7 +266,7 @@ bool heal_monster(monster& patient, int amount)
  * come up distinct. The formula below computes the probability of rolling two
  * numbers that are both large enough, minus the probability they are the same.
  */
-int _pacify_chance(const monster_info& mi, const int pow, int scale)
+static int _pacify_chance(const monster_info& mi, const int pow, int scale)
 {
     const int sides = _pacification_sides(mi.type, pow);
     const int target = mons_avg_hp(mi.type);
@@ -278,7 +278,7 @@ int _pacify_chance(const monster_info& mi, const int pow, int scale)
          / (sides * sides);
 }
 
-vector<string> _desc_pacify_chance(const monster_info& mi, const int pow)
+static vector<string> _desc_pacify_chance(const monster_info& mi, const int pow)
 {
     vector<string> descs;
 
