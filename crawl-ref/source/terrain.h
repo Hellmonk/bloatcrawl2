5c77adf54d1132baebe0cfbb00193af590e1ce76
Edgar A. Bering IV <trizor@gmail.com>
2018-11-30 15:42:51 -0500

    Don't grant explore piety for certain terrain
    Granting exploration piety for deep water, lava, and "endless" features
    encouraged players with flight or deep water swimmers to wander around
    the non-threatening boundaries of shoals and certain lava vaults.
    
    Granting exploration piety in the Temple encouraged a player to
    autoexplore the threat-free temple.

--- crawl-ref/source/terrain.h
+++ crawl-ref/source/terrain.h
@@ -140,6 +140,7 @@ void destroy_wall(const coord_def& p);
 void set_terrain_changed(const coord_def c);
 bool cell_is_clingable(const coord_def pos);
 bool cell_can_cling_to(const coord_def& from, const coord_def to);
+bool cell_triggers_conduct(const coord_def pos);
 bool is_boring_terrain(dungeon_feature_type feat);
 
 void mutate_terrain_change_duration(coord_def pos, int delta, bool replace = false);
