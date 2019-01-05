rocheio/crawl

Render menus with new UI framework
This commit replaces most of the menu rendering code with a newer
implementation.

- MenuDisplay abstraction replaced with a single UIMenu class. This
class implements the menu widget used by the UI framework, for both
tiles and console builds.
- tilereg-menu has been merged into UIMenu.
- Misc. other code improvements and bug fixes.

Most of the UIMenu implementation is fairly straightforward; the only
complication is its width-for-height nature. The menu UI will use one
column if there is enough vertical space to display all items, falling
back to a two-column layout otherwise, and so the menu's provisional
height must be known to calculate its corresponding width. This is
incompatible with the UI framework, which can only accomodate widgets
whose height varies based on their width. To work around this, the menu
widget sets itself to expand vertically to determine the available
height; it then disables vertical expansion and requests re-layout,
using the determined height in the second layout pass.

crawl-ref/source/ui.cc
@@ -223,23 +223,19 @@ void UI::_set_parent(UI* p)
     m_parent = p;
 }
 
-void UI::_invalidate_sizereq()
+void UI::_invalidate_sizereq(bool immediate)
 {
-    cached_sr_valid[0] = false;
-    cached_sr_valid[1] = false;
-    if (m_parent)
-        m_parent->_invalidate_sizereq();
-    ui_root.queue_layout();
+    for (UI* w = this; w && !w->alloc_queued; w = w->m_parent)
+        fill(begin(w->cached_sr_valid), end(w->cached_sr_valid), false);
+    if (immediate)
+        ui_root.queue_layout();
 }
 
-void UI::_queue_allocation()
+void UI::_queue_allocation(bool immediate)
 {
-    if (alloc_queued)
-        return;
-    alloc_queued = true;
-    if (m_parent)
-        m_parent->_queue_allocation();
-    else
+    for (UI* w = this; w && !w->alloc_queued; w = w->m_parent)
+        w->alloc_queued = true;
+    if (immediate)
         ui_root.queue_layout();
 }
 

Hellmonk/bloatcrawl2

(cherry picked from commit b56509fae656b24b0ec7dbb8d85bb44dee711b4f)

crawl-ref/source/ui.cc
@@ -225,23 +225,19 @@ void Widget::_set_parent(Widget* p)
     m_parent = p;
 }
 
-void Widget::_invalidate_sizereq()
+void Widget::_invalidate_sizereq(bool immediate)
 {
-    cached_sr_valid[0] = false;
-    cached_sr_valid[1] = false;
-    if (m_parent)
-        m_parent->_invalidate_sizereq();
-    ui_root.queue_layout();
+    for (auto w = this; w && !w->alloc_queued; w = w->m_parent)
+        fill(begin(w->cached_sr_valid), end(w->cached_sr_valid), false);
+    if (immediate)
+        ui_root.queue_layout();
 }
 
-void Widget::_queue_allocation()
+void Widget::_queue_allocation(bool immediate)
 {
-    if (alloc_queued)
-        return;
-    alloc_queued = true;
-    if (m_parent)
-        m_parent->_queue_allocation();
-    else
+    for (auto w = this; w && !w->alloc_queued; w = w->m_parent)
+        w->alloc_queued = true;
+    if (immediate)
         ui_root.queue_layout();
 }
 