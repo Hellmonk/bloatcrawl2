crawl/crawl

Add widget preferred size caching
This commit caches the result of a widget's calculations for its
preferred width, as well as its preferred height for a given width.

The cache for a given widget must be invalidated if it or any of its
descendants change any properties that affect their layout.

crawl-ref/source/ui.h
@@ -53,7 +53,7 @@ typedef enum {
 class UI
 {
 public:
-    UI() : margin({0,0,0,0}), flex_grow(0), align_self(UI_ALIGN_UNSET), expand_h(false), expand_v(false) {};
+    UI() : margin({0,0,0,0}), flex_grow(0), align_self(UI_ALIGN_UNSET), expand_h(false), expand_v(false), cached_sr_valid{false, false} {};
 
     i4 margin;
     int flex_grow;
@@ -67,12 +67,18 @@ public:
     // Wrapper functions which handle common behavior
     // - margins
     // - debug drawing
+    // - caching
     void render();
     UISizeReq get_preferred_size(int dim, int prosp_width);
     void allocate_region(i4 region);
 
 protected:
     i4 m_region;
+
+private:
+    bool cached_sr_valid[2];
+    UISizeReq cached_sr[2];
+    int cached_sr_pw;
 };
 
 // Box widget: similar to the CSS flexbox (without wrapping)

Hellmonk/bloatcrawl2

(cherry picked from commit dcabc23490614ed6a25f66e8f7e8c5b541f9bf28)

crawl-ref/source/ui.h
@@ -68,12 +68,18 @@ public:
 
     // Wrapper functions which handle common behavior
     // - margins
+    // - caching
     void render();
     SizeReq get_preferred_size(Direction dim, int prosp_width);
     void allocate_region(i4 region);
 
 protected:
     i4 m_region;
+
+private:
+    bool cached_sr_valid[2] = { false, false };
+    SizeReq cached_sr[2];
+    int cached_sr_pw;
 };
 
 // Box widget: similar to the CSS flexbox (without wrapping)