SeeDBe/crawl

Add UIPopup widget

crawl-ref/source/ui.h
@@ -409,6 +409,21 @@ private:
     int m_scroll;
 };
 
+class UIPopup : public UIBin
+{
+public:
+    UIPopup(shared_ptr<UI> child);
+    virtual void _render() override;
+    virtual UISizeReq _get_preferred_size(int dim, int prosp_width) override;
+    virtual void _allocate_region() override;
+
+protected:
+    shared_ptr<UI> m_root;
+#ifdef USE_TILE_LOCAL
+    ShapeBuffer m_buf;
+#endif
+};
+
 void ui_push_layout(shared_ptr<UI> root);
 void ui_pop_layout();
 void ui_pump_events();

Hellmonk/bloatcrawl2

(cherry picked from commit 8742d9a70cd43dc120aa8a3780d9ae098c40e5a6)

crawl-ref/source/ui.h
@@ -497,6 +497,21 @@ protected:
     int m_scroll = 0;
 };
 
+class Popup : public Bin
+{
+public:
+    Popup(shared_ptr<Widget> child);
+    virtual void _render() override;
+    virtual SizeReq _get_preferred_size(Direction dim, int prosp_width) override;
+    virtual void _allocate_region() override;
+
+protected:
+    shared_ptr<Widget> m_root;
+#ifdef USE_TILE_LOCAL
+    ShapeBuffer m_buf;
+#endif
+};
+
 void push_layout(shared_ptr<Widget> root);
 void pop_layout();
 void pump_events();