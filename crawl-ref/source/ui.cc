pubby/crawl

Refactor and improve Menu UI implementation
This commit breaks up the functionality previously implemented by the
UIMenu widget. Scrolling is now implemented with the UIScroller widget,
which provides a more consistent interface, and menu title and more
text are rendered with UIText widgets. In addition, the visual style of
menu items has been updated and improved, with some rendering fixes and
better margins; we now also have mouse cursor effects!

It's not all good news, though; unfortunately, the restart-allocation
issue is back with a vengeance. This is necessitated by the desire to
use one column when there's available space. While this was previously
manageable due to UIMenu rendering the entire menu (including title and
more), it's now complicated by rendering being split among multiple
widgets.

This complexity is partly caused by a mismatch between the constraints
of the widget layout system and by the desired behaviour of the menu UI.
A layout algorithm like Flutter's might be a better match, as it allows
widget children to decide their size, rather than the parent choosing
the size of its children. It's unclear whether there might be other
areas of crawl UI that are a poor fit for the Flutter model, however,
and at this point, with the complex bit written (and hopefully bug
free), I'd prefer to stick with what's already implemented and working.

crawl-ref/source/ui.cc
@@ -258,7 +258,7 @@ void UI::_set_parent(UI* p)
 
 void UI::_invalidate_sizereq(bool immediate)
 {
-    for (UI* w = this; w && !w->alloc_queued; w = w->m_parent)
+    for (UI* w = this; w; w = w->m_parent)
         fill(begin(w->cached_sr_valid), end(w->cached_sr_valid), false);
     if (immediate)
         ui_root.queue_layout();
@@ -1319,6 +1319,20 @@ void UIPopup::_allocate_region()
     m_root->allocate_region(m_region);
 }
 
+i2 UIPopup::get_max_child_size()
+{
+#ifdef USE_TILE_LOCAL
+    const auto td = tile_def(TILEG_SKIN_BOX, TEX_GUI);
+    const tile_info &ti = tiles.get_image_manager()->tile_def_info(td);
+    return {
+        (m_region[2] - 2*ti.width - 15*2) & ~0x1,
+        (m_region[3] - 2*ti.height - 15*2) & ~0x1,
+    };
+#else
+    return { m_region[2], m_region[3] };
+#endif
+}
+
 #ifdef USE_TILE_LOCAL
 void UIDungeon::_render()
 {
@@ -1399,7 +1413,13 @@ void UIRoot::layout()
 #else
         m_region = {0, 0, m_w, m_h};
 #endif
-        m_root.allocate_region({0, 0, width, height});
+        try
+        {
+            m_root.allocate_region({0, 0, width, height});
+        }
+        catch (const RestartAllocation &ex)
+        {
+        }
 
 #ifdef USE_TILE_LOCAL
         int x, y;

Hellmonk/bloatcrawl2

(cherry picked from commit 954b27418fc44cf181efa05b76dc9da11ac43fa5)

crawl-ref/source/ui.cc
@@ -263,7 +263,7 @@ void Widget::_set_parent(Widget* p)
 
 void Widget::_invalidate_sizereq(bool immediate)
 {
-    for (auto w = this; w && !w->alloc_queued; w = w->m_parent)
+    for (auto w = this; w; w = w->m_parent)
         fill(begin(w->cached_sr_valid), end(w->cached_sr_valid), false);
     if (immediate)
         ui_root.queue_layout();
@@ -1326,6 +1326,20 @@ void Popup::_allocate_region()
     m_root->allocate_region(m_region);
 }
 
+i2 Popup::get_max_child_size()
+{
+#ifdef USE_TILE_LOCAL
+    const auto td = tile_def(TILEG_SKIN_BOX, TEX_GUI);
+    const tile_info &ti = tiles.get_image_manager()->tile_def_info(td);
+    return {
+        (m_region[2] - 2*ti.width - 15*2) & ~0x1,
+        (m_region[3] - 2*ti.height - 15*2) & ~0x1,
+    };
+#else
+    return { m_region[2], m_region[3] };
+#endif
+}
+
 #ifdef USE_TILE_LOCAL
 void Dungeon::_render()
 {
@@ -1406,7 +1420,13 @@ void UIRoot::layout()
 #else
         m_region = {0, 0, m_w, m_h};
 #endif
-        m_root.allocate_region({0, 0, width, height});
+        try
+        {
+            m_root.allocate_region({0, 0, width, height});
+        }
+        catch (const RestartAllocation &ex)
+        {
+        }
 
 #ifdef USE_TILE_LOCAL
         int x, y;