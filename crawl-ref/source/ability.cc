crawl/crawl

Swap Fedhas' 1* and 2* abilities (hellmonk)
Fedhas current 1* ability, Evolution, is one of the strongest god
abilities in the game. While Fedhas isn't the only god that provides
very strong abilities early on, it's one of the gods most in need of a
rework. The most dominant Fedhas abilities create permanent allies that
require a great deal of luring to use most effectively. Until we can
rework the god, it's nice to make some balance tweaks so that players
don't feel so strongly compelled to worship Fedhas in order to win.

This commit makes Sunlight the 1* ability and Evolution the 2* ability.
Sunlight is a useful early accuracy boost that doesn't cost piety, hence
is reasonable for that piety slot. Evolution remains just as good as it
was before and will be an outstanding 2* ability.

crawl-ref/source/ability.cc
@@ -500,10 +500,10 @@ static const ability_def Ability_List[] =
     // Fedhas
     { ABIL_FEDHAS_FUNGAL_BLOOM, "Fungal Bloom",
       0, 0, 0, 0, {fail_basis::invo}, abflag::none },
-    { ABIL_FEDHAS_EVOLUTION, "Evolution",
-      2, 0, 0, 0, {fail_basis::invo, 30, 6, 20}, abflag::rations_or_piety },
     { ABIL_FEDHAS_SUNLIGHT, "Sunlight",
       2, 0, 50, 0, {fail_basis::invo, 30, 6, 20}, abflag::none },
+    { ABIL_FEDHAS_EVOLUTION, "Evolution",
+      2, 0, 0, 0, {fail_basis::invo, 30, 6, 20}, abflag::rations_or_piety },
     { ABIL_FEDHAS_PLANT_RING, "Growth",
       2, 0, 0, 0, {fail_basis::invo, 40, 5, 20}, abflag::rations },
     { ABIL_FEDHAS_SPAWN_SPORES, "Reproduction",

Hellmonk/bloatcrawl2

(cherry picked from commit b5684aae0e92d338bae3ff675c8410ac74522c94)

crawl-ref/source/ability.cc
@@ -500,10 +500,10 @@ static const ability_def Ability_List[] =
     // Fedhas
     { ABIL_FEDHAS_FUNGAL_BLOOM, "Fungal Bloom",
       0, 0, 0, 0, {fail_basis::invo}, abflag::none },
-    { ABIL_FEDHAS_EVOLUTION, "Evolution",
-      2, 0, 0, 0, {fail_basis::invo, 30, 6, 20}, abflag::variable_fruit },
     { ABIL_FEDHAS_SUNLIGHT, "Sunlight",
       2, 0, 50, 0, {fail_basis::invo, 30, 6, 20}, abflag::none },
+    { ABIL_FEDHAS_EVOLUTION, "Evolution",
+      2, 0, 0, 0, {fail_basis::invo, 30, 6, 20}, abflag::variable_fruit },
     { ABIL_FEDHAS_PLANT_RING, "Growth",
       2, 0, 0, 0, {fail_basis::invo, 40, 5, 20}, abflag::fruit },
     { ABIL_FEDHAS_SPAWN_SPORES, "Reproduction",