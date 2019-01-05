diddlyPop/crawl

Remove CRT menu attaching functionality

crawl-ref/source/tilereg-crt.cc
@@ -9,7 +9,7 @@
 #include "tilefont.h"
 #include "viewgeom.h"
 
-CRTRegion::CRTRegion(FontWrapper *font) : TextRegion(font), m_attached_menu(nullptr)
+CRTRegion::CRTRegion(FontWrapper *font) : TextRegion(font)
 {
 }
 
@@ -20,20 +20,14 @@ CRTRegion::~CRTRegion()
 
 int CRTRegion::handle_mouse(MouseEvent &event)
 {
-    int ret_val = 0;
-    if (m_attached_menu == nullptr)
+    if (event.event == MouseEvent::PRESS)
     {
-        if (event.event == MouseEvent::PRESS)
-        {
-            if (event.button == MouseEvent::LEFT)
-                ret_val = CK_MOUSE_CLICK;
-            else if (event.button == MouseEvent::RIGHT)
-                ret_val = CK_MOUSE_CMD;
-        }
+        if (event.button == MouseEvent::LEFT)
+            return CK_MOUSE_CLICK;
+        else if (event.button == MouseEvent::RIGHT)
+            return CK_MOUSE_CMD;
     }
-    else
-        ret_val = m_attached_menu->handle_mouse(event);
-    return ret_val;
+    return 0;
 }
 
 void CRTRegion::on_resize()
@@ -41,41 +35,12 @@ void CRTRegion::on_resize()
     TextRegion::on_resize();
     crawl_view.termsz.x = mx;
     crawl_view.termsz.y = my;
-
-    // Todo: resize attached menu
-}
-
-void CRTRegion::clear()
-{
-    // clear all the texts
-    TextRegion::clear();
-    detach_menu();
 }
 
 void CRTRegion::render()
 {
     set_transform(true);
-
-    // render all the inherited texts
     TextRegion::render();
-
-    // render the attached menu if it exists
-    if (m_attached_menu != nullptr)
-        m_attached_menu->draw_menu();
-}
-
-void CRTRegion::attach_menu(PrecisionMenu* menu)
-{
-    detach_menu();
-
-    m_attached_menu = menu;
-}
-
-void CRTRegion::detach_menu()
-{
-    // Tiles has no rights over the menu, thus the user must delete it
-    // Via other means
-    m_attached_menu = nullptr;
 }
 
 #endif

Hellmonk/bloatcrawl2

(cherry picked from commit f9c367dce0a84dcaad048913bb806e2bb0276fbb)

crawl-ref/source/tilereg-crt.cc
@@ -9,7 +9,7 @@
 #include "tilefont.h"
 #include "viewgeom.h"
 
-CRTRegion::CRTRegion(FontWrapper *font_arg) : TextRegion(font_arg), m_attached_menu(nullptr)
+CRTRegion::CRTRegion(FontWrapper *font_arg) : TextRegion(font_arg)
 {
 }
 
@@ -20,20 +20,14 @@ CRTRegion::~CRTRegion()
 
 int CRTRegion::handle_mouse(MouseEvent &event)
 {
-    int ret_val = 0;
-    if (m_attached_menu == nullptr)
+    if (event.event == MouseEvent::PRESS)
     {
-        if (event.event == MouseEvent::PRESS)
-        {
-            if (event.button == MouseEvent::LEFT)
-                ret_val = CK_MOUSE_CLICK;
-            else if (event.button == MouseEvent::RIGHT)
-                ret_val = CK_MOUSE_CMD;
-        }
+        if (event.button == MouseEvent::LEFT)
+            return CK_MOUSE_CLICK;
+        else if (event.button == MouseEvent::RIGHT)
+            return CK_MOUSE_CMD;
     }
-    else
-        ret_val = m_attached_menu->handle_mouse(event);
-    return ret_val;
+    return 0;
 }
 
 void CRTRegion::on_resize()
@@ -41,41 +35,12 @@ void CRTRegion::on_resize()
     TextRegion::on_resize();
     crawl_view.termsz.x = mx;
     crawl_view.termsz.y = my;
-
-    // Todo: resize attached menu
-}
-
-void CRTRegion::clear()
-{
-    // clear all the texts
-    TextRegion::clear();
-    detach_menu();
 }
 
 void CRTRegion::render()
 {
     set_transform(true);
-
-    // render all the inherited texts
     TextRegion::render();
-
-    // render the attached menu if it exists
-    if (m_attached_menu != nullptr)
-        m_attached_menu->draw_menu();
-}
-
-void CRTRegion::attach_menu(PrecisionMenu* menu)
-{
-    detach_menu();
-
-    m_attached_menu = menu;
-}
-
-void CRTRegion::detach_menu()
-{
-    // Tiles has no rights over the menu, thus the user must delete it
-    // Via other means
-    m_attached_menu = nullptr;
 }
 
 #endif