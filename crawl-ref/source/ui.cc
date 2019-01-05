crawl/crawl

Add UI layout invalidation

crawl-ref/source/ui.cc
@@ -51,7 +51,7 @@ static void ui_clear_text_region(i4 region);
 static struct UIRoot
 {
 public:
-    UIRoot() : m_child(nullptr), m_dirty(false) {};
+    UIRoot() : m_child(nullptr), m_needs_layout(false) {};
     void set_child(shared_ptr<UI> child);
 
     void resize(int w, int h);
@@ -59,12 +59,13 @@ public:
     void render();
 
     void on_event(UIEvent event);
+    void invalidate_sizereq() { m_needs_layout = true; };
 
 protected:
     int m_w, m_h;
     i4 m_region;
     shared_ptr<UI> m_child;
-    bool m_dirty;
+    bool m_needs_layout;
 } ui_root;
 
 static stack<i4> scissor_stack;
@@ -106,7 +107,6 @@ UISizeReq UI::get_preferred_size(int dim, int prosp_width)
     ASSERT(dim == 0 || dim == 1);
     ASSERT((dim == 0) == (prosp_width == -1));
 
-    // XXX: This needs invalidation on widget/descendant property change!
     if (cached_sr_valid[dim] && (!dim || cached_sr_pw == prosp_width))
         return cached_sr[dim];
 
@@ -154,9 +154,25 @@ void UI::_allocate_region()
 {
 }
 
+void UI::_set_parent(UI* p)
+{
+    m_parent = p;
+}
+
+void UI::_invalidate_sizereq()
+{
+    cached_sr_valid[0] = false;
+    cached_sr_valid[1] = false;
+    if (m_parent)
+        m_parent->_invalidate_sizereq();
+    ui_root.invalidate_sizereq();
+}
+
 void UIBox::add_child(shared_ptr<UI> child)
 {
+    child->_set_parent(this);
     m_children.push_back(move(child));
+    _invalidate_sizereq();
 }
 
 void UIBox::_render()
@@ -317,6 +333,7 @@ void UIText::set_text(const formatted_string &fs)
 {
     m_text.clear();
     m_text += fs;
+    _invalidate_sizereq();
     m_wrapped_size = { -1, -1 };
     _allocate_region();
 }
@@ -429,6 +446,7 @@ void UIImage::set_tile(tile_def tile)
     m_tw = ti.width;
     m_th = ti.height;
     m_using_tile = true;
+    _invalidate_sizereq();
 #endif
 }
 
@@ -442,6 +460,7 @@ void UIImage::set_file(string img_path)
 
     m_tw = m_img.orig_width();
     m_th = m_img.orig_height();
+    _invalidate_sizereq();
 #endif
 }
 
@@ -500,7 +519,9 @@ UISizeReq UIImage::_get_preferred_size(int dim, int prosp_width)
 
 void UIStack::add_child(shared_ptr<UI> child)
 {
+    child->_set_parent(this);
     m_children.push_back(move(child));
+    _invalidate_sizereq();
 }
 
 void UIStack::_render()
@@ -542,10 +563,12 @@ void UIStack::on_event(UIEvent event)
 
 void UIGrid::add_child(shared_ptr<UI> child, int x, int y, int w, int h)
 {
+    child->_set_parent(this);
     child_info ch = { {x, y}, {w, h} };
     m_child_info.push_back(ch);
     m_children.push_back(child);
     m_track_info_dirty = true;
+    _invalidate_sizereq();
 }
 
 void UIGrid::init_track_info()
@@ -706,7 +729,9 @@ void UIScroller::set_scroll(int y)
 
 void UIScroller::set_child(shared_ptr<UI> child)
 {
+    child->_set_parent(this);
     m_child = move(child);
+    _invalidate_sizereq();
 }
 
 void UIScroller::_render()
@@ -775,6 +800,7 @@ void UIMenu::add_entry(shared_ptr<UIMenuEntry> entry)
     for (int x = 0; x < (int)entry->widgets.size(); x++)
         m_items.add_child(move(entry->widgets[x]), x, y);
     m_entries.emplace_back(entry);
+    _invalidate_sizereq();
 }
 
 void UIMenu::scroll_selected_entry_into_view()
@@ -842,7 +868,7 @@ void UIMenu::on_event(UIEvent event)
 void UIRoot::set_child(shared_ptr<UI> ch)
 {
     m_child = move(ch);
-    m_dirty = true;
+    m_needs_layout = true;
 }
 
 void UIRoot::resize(int w, int h)
@@ -852,14 +878,14 @@ void UIRoot::resize(int w, int h)
 
     m_w = w;
     m_h = h;
-    m_dirty = true;
+    m_needs_layout = true;
 }
 
 void UIRoot::layout()
 {
-    if (!m_dirty)
+    if (!m_needs_layout || !m_child)
         return;
-    m_dirty = false;
+    m_needs_layout = false;
 
     // Find preferred size with height-for-width
     UISizeReq sr_horz = m_child->get_preferred_size(0, -1);

crawl-ref/source/ui.h
@@ -109,7 +109,7 @@ protected:
 class UI
 {
 public:
-    UI() : margin({0,0,0,0}), flex_grow(0), align_self(UI_ALIGN_UNSET), expand_h(false), expand_v(false), cached_sr_valid{false, false} {};
+    UI() : margin({0,0,0,0}), flex_grow(0), align_self(UI_ALIGN_UNSET), expand_h(false), expand_v(false), cached_sr_valid{false, false}, m_parent(nullptr) {};
     ~UI() {
         UI::slots.key_event.remove_by_target(this);
     }
@@ -122,6 +122,8 @@ public:
     virtual void _render() = 0;
     virtual UISizeReq _get_preferred_size(int dim, int prosp_width);
     virtual void _allocate_region();
+    void _set_parent(UI* p);
+    void _invalidate_sizereq();
 
     // Wrapper functions which handle common behavior
     // - margins
@@ -164,6 +166,7 @@ private:
     bool cached_sr_valid[2];
     UISizeReq cached_sr[2];
     int cached_sr_pw;
+    UI* m_parent;
 };
 
 // Box widget: similar to the CSS flexbox (without wrapping)

Hellmonk/bloatcrawl2

(cherry picked from commit 7558cdac293e1a8f78ec699b937f443bfc59854f)

crawl-ref/source/ui.cc
@@ -55,12 +55,13 @@ public:
     void render();
 
     bool on_event(const wm_event& event);
+    void queue_layout() { m_needs_layout = true; };
 
 protected:
     int m_w, m_h;
     i4 m_region;
     Stack m_root;
-    bool m_dirty{false};
+    bool m_needs_layout{false};
 } ui_root;
 
 static stack<i4> scissor_stack;
@@ -109,7 +110,9 @@ bool Bin::on_event(const wm_event& event)
 
 void Bin::set_child(shared_ptr<Widget> child)
 {
+    child->_set_parent(this);
     m_child = move(child);
+    _invalidate_sizereq();
 }
 
 void Widget::render()
@@ -121,7 +124,6 @@ SizeReq Widget::get_preferred_size(Direction dim, int prosp_width)
 {
     ASSERT((dim == HORZ) == (prosp_width == -1));
 
-    // XXX: This needs invalidation on widget/descendant property change!
     if (cached_sr_valid[dim] && (!dim || cached_sr_pw == prosp_width))
         return cached_sr[dim];
 
@@ -177,9 +179,25 @@ void Widget::_allocate_region()
 {
 }
 
+void Widget::_set_parent(Widget* p)
+{
+    m_parent = p;
+}
+
+void Widget::_invalidate_sizereq()
+{
+    cached_sr_valid[0] = false;
+    cached_sr_valid[1] = false;
+    if (m_parent)
+        m_parent->_invalidate_sizereq();
+    ui_root.queue_layout();
+}
+
 void Box::add_child(shared_ptr<Widget> child)
 {
+    child->_set_parent(this);
     m_children.push_back(move(child));
+    _invalidate_sizereq();
 }
 
 void Box::_render()
@@ -332,6 +350,7 @@ void Text::set_text(const formatted_string &fs)
 {
     m_text.clear();
     m_text += fs;
+    _invalidate_sizereq();
     m_wrapped_size = { -1, -1 };
     _allocate_region();
 }
@@ -492,6 +511,7 @@ void Image::set_tile(tile_def tile)
     const tile_info &ti = tiles.get_image_manager()->tile_def_info(m_tile);
     m_tw = ti.width;
     m_th = ti.height;
+    _invalidate_sizereq();
 #endif
 }
 
@@ -529,7 +549,9 @@ SizeReq Image::_get_preferred_size(Direction dim, int prosp_width)
 
 void Stack::add_child(shared_ptr<Widget> child)
 {
+    child->_set_parent(this);
     m_children.push_back(move(child));
+    _invalidate_sizereq();
 }
 
 void Stack::pop_child()
@@ -537,6 +559,7 @@ void Stack::pop_child()
     if (!m_children.size())
         return;
     m_children.pop_back();
+    _invalidate_sizereq();
 }
 
 void Stack::_render()
@@ -581,9 +604,11 @@ bool Stack::on_event(const wm_event& event)
 
 void Grid::add_child(shared_ptr<Widget> child, int x, int y, int w, int h)
 {
+    child->_set_parent(this);
     child_info ch = { {x, y}, {w, h}, move(child) };
     m_child_info.push_back(ch);
     m_track_info_dirty = true;
+    _invalidate_sizereq();
 }
 
 void Grid::init_track_info()
@@ -844,7 +869,7 @@ bool Scroller::on_event(const wm_event& event)
 void UIRoot::push_child(shared_ptr<Widget> ch)
 {
     m_root.add_child(move(ch));
-    m_dirty = true;
+    m_needs_layout = true;
 #ifndef USE_TILE_LOCAL
     if (m_root.num_children() == 1)
     {
@@ -857,7 +882,7 @@ void UIRoot::push_child(shared_ptr<Widget> ch)
 void UIRoot::pop_child()
 {
     m_root.pop_child();
-    m_dirty = true;
+    m_needs_layout = true;
 #ifndef USE_TILE_LOCAL
     if (m_root.num_children() == 0)
         clrscr();
@@ -871,28 +896,29 @@ void UIRoot::resize(int w, int h)
 
     m_w = w;
     m_h = h;
-    m_dirty = true;
+    m_needs_layout = true;
 }
 
 void UIRoot::layout()
 {
-    if (!m_dirty)
-        return;
-    m_dirty = false;
+    while (m_needs_layout)
+    {
+        m_needs_layout = false;
 
-    // Find preferred size with height-for-width: we never allocate less than
-    // the minimum size, but may allocate more than the natural size.
-    SizeReq sr_horz = m_root.get_preferred_size(Widget::HORZ, -1);
-    int width = max(sr_horz.min, m_w);
-    SizeReq sr_vert = m_root.get_preferred_size(Widget::VERT, width);
-    int height = max(sr_vert.min, m_h);
+        // Find preferred size with height-for-width: we never allocate less than
+        // the minimum size, but may allocate more than the natural size.
+        SizeReq sr_horz = m_root.get_preferred_size(Widget::HORZ, -1);
+        int width = max(sr_horz.min, m_w);
+        SizeReq sr_vert = m_root.get_preferred_size(Widget::VERT, width);
+        int height = max(sr_vert.min, m_h);
 
 #ifdef USE_TILE_LOCAL
-    m_region = {0, 0, width, height};
+        m_region = {0, 0, width, height};
 #else
-    m_region = {0, 0, m_w, m_h};
+        m_region = {0, 0, m_w, m_h};
 #endif
-    m_root.allocate_region({0, 0, width, height});
+        m_root.allocate_region({0, 0, width, height});
+    }
 }
 
 void UIRoot::render()

crawl-ref/source/ui.h
@@ -102,6 +102,8 @@ public:
     virtual void _render() = 0;
     virtual SizeReq _get_preferred_size(Direction dim, int prosp_width);
     virtual void _allocate_region();
+    void _set_parent(Widget* p);
+    void _invalidate_sizereq();
 
     // Wrapper functions which handle common behavior
     // - margins
@@ -141,6 +143,7 @@ private:
     bool cached_sr_valid[2] = { false, false };
     SizeReq cached_sr[2];
     int cached_sr_pw;
+    Widget* m_parent = nullptr;
 };
 
 class Container : public Widget