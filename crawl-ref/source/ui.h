krfreak/crawl

Add helper for running UI layers (Currency)

crawl-ref/source/ui.h
@@ -467,6 +467,7 @@ protected:
 void ui_push_layout(shared_ptr<UI> root);
 void ui_pop_layout();
 void ui_pump_events();
+void ui_run_layout(shared_ptr<UI> root, const bool& done);
 
 void ui_push_scissor(i4 scissor);
 void ui_pop_scissor();

Hellmonk/bloatcrawl2

(cherry picked from commit 6b7a2ddc87fc8e8aeb5487dc8527f1ac241d6e6d)

crawl-ref/source/ui.h
@@ -534,6 +534,7 @@ protected:
 void push_layout(shared_ptr<Widget> root);
 void pop_layout();
 void pump_events();
+void run_layout(shared_ptr<Widget> root, const bool& done);
 
 void push_scissor(i4 scissor);
 void pop_scissor();