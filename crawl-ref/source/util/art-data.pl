8e22b2d787e875902bbed0ce3190242ca63d9711
Floodkiller <floodkiller3228@gmail.com>
2019-03-04 15:46:54 -0500

    Give Misfortune Harm, not +SH
    bbuuuububububugggssss

--- crawl-ref/source/util/art-data.pl
+++ crawl-ref/source/util/art-data.pl
@@ -66,6 +66,7 @@ my %field_type = (
     RND_TELE => "bool",
     SEEINV   => "bool",
     SKIP_EGO => "bool",
+    SH       => "num",
     SLAY     => "num",
     SPECIAL  => "bool",
     SLOW     => "bool",
@@ -493,7 +494,7 @@ my @art_order = (
     "BASE_DELAY", "HP", "CLARITY", "BASE_ACC", "BASE_DAM", "\n",
     "RMSL", "FOG", "REGEN", "unused", "NO_UPGRADE", "RCORR", "\n",
     "RMUT", "unused", "CORRODE", "DRAIN", "SLOW", "FRAGILE", "\n",
-    "HARM", "\n",
+    "SH", "HARM", "\n",
     "}",
 
     "equip_func", "unequip_func", "world_reacts_func", "melee_effects_func",
