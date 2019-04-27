fa4cde94e04755e1126f7a884e8b4f331635e22e
advil <rawlins@gmail.com>
2019-04-27 13:30:09 -0400

    Change PCG initialization to something more standard
    Perhaps because of the age of our PCG implementation (it comes from
    before the official implementations were well-developed/documented), our
    initialization process was fairly ad-hoc, which is not usually a good
    idea in RNG design. This commit converts the initialization behavior to
    match what the official implementations do, for example:
    
        https://github.com/imneme/pcg-c-basic/blob/master/pcg_basic.c
    
    Possibly it would be best even to just use the official c++ PCG
    implementation, but this commit doesn't go that far.
    
    Needless to say, this change affects all seeds.

--- crawl-ref/source/tileview.cc
+++ crawl-ref/source/tileview.cc
@@ -315,9 +315,9 @@ void tile_init_flavour()
     vector<unsigned int> output;
     {
         domino::DominoSet<domino::EdgeDomino> dominoes(domino::cohen_set, 8);
-        uint64_t seed[] = { static_cast<uint64_t>(you.where_are_you ^ you.game_seed),
-            static_cast<uint64_t>(you.depth) };
-        rng::PcgRNG rng(seed, ARRAYSZ(seed));
+        rng::PcgRNG rng(
+            static_cast<uint64_t>(you.where_are_you ^ you.game_seed),
+            static_cast<uint64_t>(you.depth));
         dominoes.Generate(X_WIDTH, Y_WIDTH, output, rng);
     }
     for (rectangle_iterator ri(0); ri; ++ri)
